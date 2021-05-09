305397bb835550bad0686cc65a6f9967fb622199
Islam Thabet <ithabet.php@gmail.com>
2021-05-09 13:48:53 +0200

    login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,80 +1,118 @@
-<x-guest-layout>
-    <div class="padding-top-80">
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <title>I'M Happy - Login</title>
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="alqamah"/>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+
+    <!-- vector map CSS -->
+    <link href="{{ url('assets/dist/') }}/vendors/bower_components/jasny-bootstrap/dist/css/jasny-bootstrap.min.css" rel="stylesheet" type="text/css"/>
 
 
-        <x-jet-validation-errors class="mb-3 rounded-0" />
 
-        @if (session('status'))
-            <div class="mb-3 alert alert-success rounded-0" role="alert">
-                {{ session('status') }}
-            </div>
-        @endif
+    <!-- Custom CSS -->
+    <link href="{{ url('assets/dist/') }}/css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<!--Preloader-->
+<div class="preloader-it">
+    <div class="la-anim-1"></div>
+</div>
+<!--/Preloader-->
 
-        <div class="card-body">
-            <form method="POST" action="{{ route('login') }}">
-                @csrf
-                <div class="mb-3">
-                    <x-jet-label value="{{ __('Email') }}" />
-
-                    <x-jet-input class="{{ $errors->has('email') ? 'is-invalid' : '' }}" type="email"
-                                 name="email" :value="old('email')" required />
-                    <x-jet-input-error for="email"></x-jet-input-error>
-                </div>
-
-                <div class="mb-3">
-                    <x-jet-label value="{{ __('Password') }}" />
-
-                    <x-jet-input class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" type="password"
-                                 name="password" required autocomplete="current-password" />
-                    <x-jet-input-error for="password"></x-jet-input-error>
-                </div>
-
-                <div class="mb-3">
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                        <label class="form-check-label" for="remember">
-                            {{ __('Remember Me') }}
-                        </label>
-                    </div>
-                </div>
-
-                <div class="mb-0">
-                    <div class="d-flex justify-content-end align-items-baseline">
-                        @if (Route::has('password.request'))
-                            <a class="mr-3 text-muted" href="{{ route('password.request') }}">
-                                {{ __('Forgot your password?') }}
-                            </a>
-                        @endif
-
-                        <x-jet-button>
-                            {{ __('Login') }}
-                        </x-jet-button>
-                    </div>
-                </div>
-            </form>
+<div class="wrapper pa-0">
+    <header class="sp-header">
+        <div class="sp-logo-wrap pull-left">
+            <a href="index.html">
+                <img class="brand-img mr-10" width="80" src="{{ url('images/logos/default.png') }}" alt="brand"/>
+            </a>
         </div>
-        @if (JoelButcher\Socialstream\Socialstream::show())
-            {{-- <x-socialstream-providers /> --}}
-        @endif
-    </x-jet-authentication-card>
+        <div class="form-group mb-0 pull-right">
+            <span class="inline-block pr-10">Don't have an account?</span>
+            <a class="inline-block btn btn-info  btn-rounded btn-outline" href="{{ url('/') }}">Sign Up</a>
+        </div>
+        <div class="clearfix"></div>
+    </header>
+
+    <!-- Main Content -->
+    <div class="page-wrapper pa-0 ma-0 auth-page">
+        <div class="container-fluid">
+            <!-- Row -->
+            <div class="table-struct full-width full-height">
+                <div class="table-cell vertical-align-middle auth-form-wrap">
+                    <div class="auth-form  ml-auto mr-auto no-float">
+                        <div class="row">
+                            <div class="col-sm-12 col-xs-12">
+                                <div class="mb-30">
+                                    <h3 class="text-center txt-dark mb-10">I'M Happy Login</h3>
+                                    <h6 class="text-center nonecase-font txt-grey">Enter your details below</h6>
+                                </div>
+                                <div class="form-wrap">
+                                    @if($errors->has('email'))
+                                        <div class="alert alert-danger">{{ $errors->first('email') }}</div>
+                                    @endif
+                                    <form method="POST" action="{{ route('login') }}">
+                                        @csrf
+                                        <div class="form-group">
+                                            <label class="control-label mb-10" for="exampleInputEmail_2">Email address</label>
+                                            <input type="email" name="email" class="form-control" required="" id="exampleInputEmail_2" placeholder="Enter email">
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="pull-left control-label mb-10" for="exampleInputpwd_2">Password</label>
+                                            <a class="capitalize-font txt-primary block mb-10 pull-right font-12" href="{{ route('password.request') }}">forgot password ?</a>
+                                            <div class="clearfix"></div>
+                                            <input type="password" name="password" class="form-control" required="" id="exampleInputpwd_2" placeholder="Enter pwd">
+                                        </div>
+
+                                        <div class="form-group">
+                                            <div class="checkbox checkbox-primary pr-10 pull-left">
+                                                <input id="checkbox_2" name="remember"  type="checkbox">
+                                                <label for="checkbox_2"> Keep me logged in</label>
+                                            </div>
+                                            <div class="clearfix"></div>
+                                        </div>
+                                        <div class="form-group text-center">
+                                            <button type="submit" class="btn btn-primary btn-outline btn-rounded">sign in</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /Row -->
+        </div>
+
     </div>
-    @push('styles')
-    <style>
-        svg{
-            width: 40px;
-        }
-        .flex{
-            display:flex;
-            flex-wrap: wrap;
-            margin-left:auto;
-            margin-right:auto;
-            padding-bottom:15px;
-        }
-    </style>
-@endpush
-</x-guest-layout>
+    <!-- /Main Content -->
+
+</div>
+<!-- /#wrapper -->
+
+<!-- JavaScript -->
+
+<!-- jQuery -->
+<script src="{{ url('assets/dist/') }}/vendors/bower_components/jquery/dist/jquery.min.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="{{ url('assets/dist/') }}/vendors/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="{{ url('assets/dist/') }}/vendors/bower_components/jasny-bootstrap/dist/js/jasny-bootstrap.min.js"></script>
+
+<!-- Slimscroll JavaScript -->
+<script src="{{ url('assets/dist/') }}/js/jquery.slimscroll.js"></script>
+
+<!-- Init JavaScript -->
+<script src="{{ url('assets/dist/') }}/js/init.js"></script>
+</body>
+
+</html>
+
