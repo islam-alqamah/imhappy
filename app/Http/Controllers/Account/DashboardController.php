7ca0aa6f31837d5f8ce462f0126347fef8d1dcc1
Islam Thabet <ithabet.php@gmail.com>
2021-05-09 10:33:17 +0200

    data city

--- app/Http/Controllers/Account/DashboardController.php
+++ app/Http/Controllers/Account/DashboardController.php
@@ -343,13 +343,7 @@ if($agent->isMobile()){
                 if(isset($request->branch_id) && $request->branch_id != 'all' ){
                     $query->where('branch_id', $request->branch_id);
                 }
-                if(isset($request->date_range)){
-                    $start_date = explode(' - ',$request->date_range)[0];
-                    $end_date = explode(' - ',$request->date_range)[1];
-                    $carbon_start_date = Carbon::createFromFormat('m/d/Y', $start_date)->format('Y-m-d');
-                    $carbon_end_date = Carbon::createFromFormat('m/d/Y', $end_date)->format('Y-m-d');
-                    $query->whereBetween('created_at',[$carbon_start_date,$carbon_end_date])->get();
-                }
+
             })->get()->groupBy(function($date) {
                 return Carbon::parse($date->created_at)->format('l');
             });
@@ -446,13 +440,7 @@ if($agent->isMobile()){
                 if(isset($request->branch_id) && $request->branch_id != 'all' ){
                     $query->where('branch_id', $request->branch_id);
                 }
-                if(isset($request->date_range)){
-                    $start_date = explode(' - ',$request->date_range)[0];
-                    $end_date = explode(' - ',$request->date_range)[1];
-                    $carbon_start_date = Carbon::createFromFormat('m/d/Y', $start_date)->format('Y-m-d');
-                    $carbon_end_date = Carbon::createFromFormat('m/d/Y', $end_date)->format('Y-m-d');
-                    $query->whereBetween('created_at',[$carbon_start_date,$carbon_end_date])->get();
-                }
+
             })->get()->groupBy(function($date) {
                 return Carbon::parse($date->created_at)->format('l');
             });
@@ -576,13 +564,7 @@ if($agent->isMobile()){
                     if(isset($request->branch_id) && $request->branch_id != 'all' ){
                         $query->where('branch_id', $request->branch_id);
                     }
-                    if(isset($request->date_range)){
-                        $start_date = explode(' - ',$request->date_range)[0];
-                        $end_date = explode(' - ',$request->date_range)[1];
-                        $carbon_start_date = Carbon::createFromFormat('m/d/Y', $start_date)->format('Y-m-d');
-                        $carbon_end_date = Carbon::createFromFormat('m/d/Y', $end_date)->format('Y-m-d');
-                        $query->whereBetween('created_at',[$carbon_start_date,$carbon_end_date])->get();
-                    }
+
                 })->get()->groupBy(function($date) {
                     return Carbon::parse($date->created_at)->format('l');
                 });
